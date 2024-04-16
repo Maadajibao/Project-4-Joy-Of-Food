c1309f06fb7d9e2fd715747dd5251f35c3d560f0
Maada Jibao <maada.jibao01@gmail.com>
2024-04-16 05:48:36 +0000

    Create models reservation, add to admin

--- book_food/models.py
+++ book_food/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    party_size = models.PositiveIntegerField()
+    num_tables = models.PositiveIntegerField(default=1)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Reservation for {self.party_size} guests on {self.date} at {self.time}"
