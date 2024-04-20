f5439eae6b360d752545a248faaeed96dff253c9
Maada Jibao <maada.jibao01@gmail.com>
2024-04-20 01:37:31 +0000

    Add authorization to settings

--- food_joy/urls.py
+++ food_joy/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("book_food.urls"), name="book_food-urls"),
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
 ]
