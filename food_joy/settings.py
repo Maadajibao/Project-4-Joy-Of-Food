f27726ff6bd3ffe2b82c751a3e6cbc8a1d3bf717
Maada Jibao <maada.jibao01@gmail.com>
2024-04-25 04:35:07 +0000

    Made debug to false

--- food_joy/settings.py
+++ food_joy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-maadajibao-project4joyo-el6liml2nnr.ws-eu110.gitpod.io',
                 '.herokuapp.com']
