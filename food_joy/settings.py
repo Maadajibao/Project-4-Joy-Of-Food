b7abdd41226ec58c01942676f09bce9c1b823622
Maada Jibao <maada.jibao01@gmail.com>
2024-04-25 00:40:47 +0000

    Styled signup and login page

--- food_joy/settings.py
+++ food_joy/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-maadajibao-project4joyo-el6liml2nnr.ws-eu110.gitpod.io',
                 '.herokuapp.com']
